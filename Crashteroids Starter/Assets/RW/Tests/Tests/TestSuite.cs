e2f40fb7bc6bf18f6548d599ba668a04a48bd15d
14-KeithLai <ng_bing_hui@connect.ite.edu.sg>
2023-02-13 15:33:19 +0800

    Lab 11
    finished

--- Crashteroids Starter/Assets/RW/Tests/Tests/TestSuite.cs
+++ Crashteroids Starter/Assets/RW/Tests/Tests/TestSuite.cs
@@ -10,6 +10,67 @@ namespace Tests
     {
         private Game game;
 
+        [SetUp]
+        public void Setup()
+        {
+            GameObject gameGameObject =
+                MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+            game = gameGameObject.GetComponent<Game>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(game.gameObject);
+        }
+        [UnityTest]
+        public IEnumerator LaserMovesUp()
+        {
+            // 1
+            GameObject laser = game.GetShip().SpawnLaser();
+            // 2
+            float initialYPos = laser.transform.position.y;
+            yield return new WaitForSeconds(0.1f);
+            // 3
+            Assert.Greater(laser.transform.position.y, initialYPos);
+        }
+        [UnityTest]
+        public IEnumerator LaserDestroysAsteroid()
+        {
+            // 1
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            asteroid.transform.position = Vector3.zero;
+            GameObject laser = game.GetShip().SpawnLaser();
+            laser.transform.position = Vector3.zero;
+            yield return new WaitForSeconds(0.1f);
+            // 2
+            UnityEngine.Assertions.Assert.IsNull(asteroid);
+        }
+
+        [UnityTest]
+        public IEnumerator DestroyedAsteroidRaisesScore()
+        {
+            // 1
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            asteroid.transform.position = Vector3.zero;
+            GameObject laser = game.GetShip().SpawnLaser();
+            laser.transform.position = Vector3.zero;
+            yield return new WaitForSeconds(0.1f);
+            // 2
+            Assert.AreEqual(game.score, 1);
+        }
+        [UnityTest]
+        public IEnumerator NewGameRestartsGame()
+        {
+            //1
+            game.isGameOver = true;
+            game.NewGame();
+            //2
+            Assert.False(game.isGameOver);
+            yield return null;
+        }
+
+
         // 1
         [UnityTest]
         public IEnumerator AsteroidsMoveDown()
@@ -27,6 +88,23 @@ namespace Tests
             // 6
             Assert.Less(asteroid.transform.position.y, initialYPos);
             // 7
+            Object.Destroy(game.gameObject);
+        }
+        [UnityTest]
+        public IEnumerator GameOverOccursOnAsteroidCollision()
+        {
+            GameObject gameGameObject =
+               MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+            Game game = gameGameObject.GetComponent<Game>();
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            //1
+            asteroid.transform.position = game.GetShip().transform.position;
+            //2
+            yield return new WaitForSeconds(0.1f);
+
+            //3
+            Assert.True(game.isGameOver);
+
             Object.Destroy(game.gameObject);
         }
 
